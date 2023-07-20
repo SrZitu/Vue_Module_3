675877602eb6b8895835ef40b0c5c39e52fddceb
SrZitu <zitu094@gmail.com>
2023-07-21 02:18:26 +0600

    Your commit message here

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
     const message = 'Welcome. Get ready to master <em>Vue.js 3!</em>'
     const date = '<strong>30th June, 2023</strong>'
+    const name="Hello <em>Displaying</em> Html!"
 </script>
 
 <template>
   <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
       <h1 class="text-4xl" v-html="message"></h1>
       <h2 class="mt-10 text-xl text-gray-700">Today is <span v-html="date"></span></h2>
+      <h5 class="mt-10 text-xl text-gray-700"><span v-html="name"></span></h5>
   </div>
 </template>
 
