39a150d03b215d93bdef6eb1db11dff94c74db05
SrZitu <zitu094@gmail.com>
2023-07-21 22:16:52 +0600

    class binding

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,52 @@
 <script setup>
-const location = "Rajshahi";
-const weather = "t";
+  const bgOrange="bg-orange-400"
+  const bgIndigo="bg-indigo-400"
+  const bgGreen="bg-green-400"
+
+  const weekday = true
+
+  const colors={
+    bgColor:'bg-teal-100',
+    borderColor:'border-teal-500',
+    textColor:'text-teal-900'
+  }
+  const messageType = 'success'
+
+if('error'==messageType){
+  colors.bgColor = 'bg-red-100'
+  colors.borderColor = 'border-red-500'
+  colors.textColor = 'text-red-900'
+}
+
+if('warning'==messageType){
+  colors.bgColor = 'bg-orange-100'
+  colors.borderColor = 'border-orange-500'
+  colors.textColor = 'text-orange-900'
+}
 </script>
 
 <template>
-  <section class="container mx-auto flex items-center flex-col">
-    <h1 class="text-center text-2xl py-10">Logic in Vue.js</h1>
-    <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
-      <h1 class="text-3xl">{{ location }}</h1>
-      <template v-if="weather == 'r'">
-        <h2 class="text-2xl">Weather: Rainy</h2>
-        <img class="w-32" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png" alt="">
-      </template>
+  <section class="container mx-auto">
+    <h1 class="text-center text-3xl py-12">Class & Style Binding</h1>
 
-      <template v-if="weather == 'pc'">
-        <h2 class="text-2xl">Weather: Partly Cloudy</h2>
-        <img class="w-32" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png" alt="">
-      </template>
+    <section class="flex space-x-10">
+      <div class="w-1/3 h-16" :class="bgOrange"></div>
+      <div class="w-1/3 h-16" :class="bgIndigo" ></div>
+      <div class="w-1/3 h-16" :class="bgGreen"></div>
+    </section>
 
-      <template v-if="weather == 's'">
-        <h2 class="text-2xl">Weather: Sunny</h2>
-        <img class="w-32" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png" alt="">
-      </template>
+    <section class="mt-10">
+      <div class="h-16 flex items-center justify-center text-white" :class="[weekday?'bg-green-600':'bg-red-600',weekday==true?'text-2xl':'text-3xl']">
+        It's a weekday!
+      </div>
+    </section>
 
-      <template v-if="weather == 't'">
-        <h2 class="text-2xl">Weather: Thunderstorm</h2>
-        <img class="w-32" src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png" alt="">
-      </template>
-    </div>
+    <section class="mt-10">
+      <div class=" border-t-4 rounded-b px-4 py-3 shadow-md" :class="[colors.bgColor,colors.textColor,colors.borderColor]" >
+        <p class="font-bold">Our privacy policy has changed</p>
+        <p class="text-sm">Make sure you know how these changes affect you.</p>
+      </div>
+    </section>
   </section>
 </template>
 
